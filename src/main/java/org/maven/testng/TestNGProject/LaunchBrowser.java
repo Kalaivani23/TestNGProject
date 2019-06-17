f452fc655d14a1c9a251415e45d4f715c4de4de8
Kalaivani23 <kalaivanivelusamy2302@gmail.com>
2019-06-17 20:21:11 +0530

    TestNG Project Added

---
+++ src/main/java/org/maven/testng/TestNGProject/LaunchBrowser.java
@@ -0,0 +1,22 @@
+package org.maven.testng.TestNGProject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LaunchBrowser {
+	@Test
+	public void test1() {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\hi\\eclipse-workspace\\Kalaivani\\EmployeeDetails\\driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+		String url = driver.getCurrentUrl();
+		System.out.println(url);
+		String title = driver.getTitle();
+		System.out.println(title);
+		driver.quit();
+	}
+
+}
